35e6072bda65037899b70ce08da9300ba555501b
arie <arie@users-MacBook-Pro.local>
2017-10-25 13:33:28 +0100

    experiment with secure injection in travis

---
+++ ios_build_full.sh
@@ -0,0 +1,33 @@
+ #!/bin/bash
+
+# we then inject the variable into the appropriate line in
+# lib/utils.dart in getCipherParams
+
+# export TRAVIS=true
+# unset TRAVIS
+
+if [[ ${TRAVIS+x} ]];
+then
+  # if TRAVIS is defined we use the TRAVIS encrypted variable
+  inject=$TRAVIS_ENCRYPTED
+else
+  # else we use the non-encrypted variable
+  inject="key = new Uint8List.fromList([97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112]);"
+fi
+
+file="./lib/main.dart"
+result=$(grep -RHn --colour=auto "//MATCHTOKEN" --include=${file} .)
+array=(${result//:/ })
+line=${array[1]}
+
+echo $line
+echo $file
+echo $inject
+
+perl -pi -e 's/\/\/MATCHTOKEN/'"${inject}"'/ if $. == '"${line}"'' ${file}
+
+cat ${file}
+
+flutter packages get
+flutter build ios --no-codesign --release
+
